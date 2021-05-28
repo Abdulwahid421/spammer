9e6c52dea7f912d01cb47c2f771d33979e458625
Abdul Wahid <70502035+Abdulwahid421@users.noreply.github.com>
2021-05-28 16:11:53 +0500

    Create SpamBot.py

---
+++ SpamBot.py
@@ -0,0 +1,19 @@
+#Its time to spam
+
+
+#import relevant modules
+
+import time
+import  pyautogui
+
+
+def sendSpamBot():
+    time.sleep(5)
+
+    text = open('Spam message.txt')
+    for each_line in text:
+        pyautogui.typewrite(each_line)
+        pyautogui.press('enter')
+
+
+sendSpamBot()
